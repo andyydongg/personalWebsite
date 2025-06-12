62915771023500e994803513f9252fd17cbf54b9
Andy Dong <117495443+andyydongg@users.noreply.github.com>
2025-06-11 23:23:05 -0400

    Add files via upload

--- layout.js
+++ layout.js
@@ -1,8 +1,11 @@
 import { Analytics } from '@vercel/analytics/next';
-
+ 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
       <body>
         {children}
         <Analytics />
